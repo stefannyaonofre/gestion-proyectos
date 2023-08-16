bd0b8f5916f47d8c08ba1299d10980a3b249dcff
Stefannya <stefannyaonofre@gmail.com>
2023-08-16 11:07:02 -0500

    creaction services

--- src/pages/detalle/DetalleProyecto.jsx
+++ src/pages/detalle/DetalleProyecto.jsx
@@ -1,13 +1,51 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import "./detalleProyecto.scss";
+import { getProject } from "../../services/projectsServices";
 
 const DetalleProyecto = () => {
-
+  
   const { idProyecto } = useParams();
 
-  return (
-    <div>DetalleProyecto</div>
-  )
-}
+  useEffect(() => {
+    project()
+    console.log(idProyecto)
+  }, [])
 
-export default DetalleProyecto
+  const project = async () => {
+    try {
+      const response = await getProject(idProyecto);
+      console.log(response)
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <>
+    <section className="container-detalle">
+    <div className="card cards">
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+
+      <ul className="list-group cards">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </section>
+      
+    </>
+  );
+};
+
+export default DetalleProyecto;
---
+++ src/services/data.js
@@ -0,0 +1,6 @@
+const URL = 'https://miniback-projects.onrender.com';
+
+export const endpoints = {
+    urlProjects: `${URL}/projects`,
+    urlTareas: `${URL}/tarea`
+}
