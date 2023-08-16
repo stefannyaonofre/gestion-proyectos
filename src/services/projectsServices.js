d5a2599ecdaaf40ae70783ff2cb50e332983cca6
Alice <ann.bondarets22@gmail.com>
2023-08-16 12:53:49 -0500

    :sparkles:Finished home component

--- src/services/projectsServices.js
+++ src/services/projectsServices.js
@@ -11,3 +11,36 @@ export const getProject = async (idProject) => {
         return {}
     }
 }
+
+export const getAllProjects = async () => {
+    try {
+        const { data } = await axios.get(`${endpoints.urlProjects}`);
+        return data
+        
+    } catch (error) {
+        console.log(error)
+        return {}
+    }
+}
+
+export const deleteProject = async (id) => {
+    try {
+        const response = await axios.delete(`${endpoints.urlProjects}/${id}`);
+        return response
+        
+    } catch (error) {
+        console.log(error)
+        return error
+    }
+}
+
+export const createProject = async (newProject) => {
+    try {
+        const response = await axios.post(`${endpoints.urlProjects}`, newProject);
+        return response
+        
+    } catch (error) {
+        console.log(error)
+        return error
+    }
+}
